9b8f8186a24865989eb65880b5ec256f2817afaa
louisgendron26 <louisg.212@gmail.com>
2021-11-21 14:32:52 -0500

    Quasar plugin as UI framework

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-toolbar-title>
+          PubWeave
+        </q-toolbar-title>
+
+        <q-chip
+          dense
+          outline
+          size='lg'
+          id='wallet-chip'
+          text-color="white"
+          icon='fas fa-wallet'
+          style="display: none">
+        </q-chip>
+
+        <q-btn
+          label='Login'
+          id='header-login'
+          @click="loginFunc"
+        />
+
+        <q-btn
+          label='Logout'
+          id='header-logout'
+          @click="logoutFunc"
+          style='display: none'
+        />
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Gallery" />
+        <q-route-tab to="/upload" label="Create NFT" />
+        <q-route-tab to="/my-assets" label="My Assets" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <q-page>
+        <Home />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'LayoutDefault',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    Home
+  },
+  methods: {
+    loginFunc: async () => {
+      await window.arweaveWallet.connect([
+        'ACCESS_ADDRESS',
+        'SIGN_TRANSACTION'
+      ], {
+        name: 'PubWeave'
+      })
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+      try {
+        const address = await window.arweaveWallet.getActiveAddress()
+        document.querySelector('#header-login').style.display = 'none'
+        document.querySelector('#header-logout').style.display = 'block'
+        document.querySelector('#wallet-chip').style.display = 'block'
+        document.querySelector('#wallet-chip').innerHTML = address.substring(0, 10) + '...'
+      } catch (e) {
+        document.querySelector('#header-login').style.display = 'block'
+        document.querySelector('#header-logout').style.display = 'none'
+        document.querySelector('#wallet-chip').innerHTML = ''
+        document.querySelector('#wallet-chip').display = 'none'
+
+      }
+    },
+    logoutFunc: async () => {
+      await window.arweaveWallet.disconnect()
+      document.querySelector('#header-login').style.display = 'block'
+      document.querySelector('#header-logout').style.display = 'none'
+      document.querySelector('#wallet-chip').innerHTML = ''
+      document.querySelector('#wallet-chip').display = 'none'
+    }
+  },
+  setup () {
+    return {
+      // leftDrawerOpen: ref(false)
+    }
+  }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <ArLogin />
     <List />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import ArLogin from '@/components/ArLogin.vue'
 import List from '@/components/List.vue'
 
 export default {
   name: 'Home',
   components: {
-    ArLogin,
     List
   },
   
